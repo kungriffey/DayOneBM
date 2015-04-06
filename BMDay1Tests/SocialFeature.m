88284a7e0ef2cf34696632fa903018652ccff91b
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-06 15:50:14 -0400

    Added Feature Model

---
+++ BMDay1Tests/SocialFeature.m
@@ -0,0 +1,13 @@
+//
+//  SocialFeature.m
+//  BMDay1
+//
+//  Created by Kunwardeep Gill on 2015-04-06.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import "SocialFeature.h"
+
+@implementation SocialFeature
+
+@end
